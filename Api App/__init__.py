73ff326b6a3e139f8b6d0db309b3a7a9788bd17b
Humberto Kuruklis <hakuruklis@gmail.com>
2018-11-15 15:41:01 -0500

    Creando Api

---
+++ Api App/__init__.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient
+from bson.json_util import dumps
+import pymongo
+import json
+
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+
+client = pymongo.MongoClient("mongodb+srv://atlasadmin:atlasadmin@transacciones-v8fmm.mongodb.net/test?retryWrites=true")
+db = client["Transacciones"]
+
+collection = db["Transacciones"]
+
+
+@app.route("/api/v1/", methods=['POST','GET'])
+def usuarios():
+    response = {}
+    data = dumps(collection.find_one())
+    response["data"] = data
+    return jsonify(response)
+
+
+@app.route("/api/v1/<int:usuario>", methods=['POST','GET'])
+def buscarUsuario(usuario):
+    response = {}
+    #data = db.client.find_one({"id_tran":usuario})
+    data = dumps(collection.find({"id_tran":usuario}))
+    if data:
+        response["data"] = data
+    else:
+        response["mensaje"] = "El usuario no existe"
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
